00e0461f650b7cc094b4b6053dcd82dca5ce1b96
burrhannes <johannes.burrtu-dortmund.de>
2023-05-31 13:44:50 +0200

    raise exception for incorrect inputs, added doc string

--- app.py
+++ app.py
@@ -37,16 +37,29 @@ def haversine(longitude1, latitude1, longitude2, latitude2, metric="km"):
 app = Flask(__name__)
 
 @app.route('/api/v1/distance/<string_1>/<string_2>', methods=['GET'])
-def calculate_distance(string_1, string_2):
+def create_response(string_1, string_2):
+    """
+    Function creating the response for the API
+    takes two strings, which need to be the abrevations
+    matching the database, then calculates the distance,
+    and creates the response
+    """
 
+    # select both stations based on accronym given
     bahnhof1 = df[df["DS100"]==string_1]
     bahnhof2 = df[df["DS100"]==string_2]
 
+    # raise exception if one input does not match
+    if len(bahnhof1)+len(bahnhof2)<2:
+        raise Exception
+
+    # calculate beeline between stations via haversine formula
     distance = haversine(bahnhof1["Laenge"].item(),
                         bahnhof1["Breite"].item(),
                         bahnhof2["Laenge"].item(),
                         bahnhof2["Breite"].item())
 
+    # define json response
     response = {
         'from': bahnhof1["NAME"].item(),
         'to': bahnhof2["NAME"].item(),
@@ -57,5 +70,5 @@ def calculate_distance(string_1, string_2):
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
